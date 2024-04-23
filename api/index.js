b4aed5a03cc0b60b59aa0c2b19ac9faaa71a2bdd
Gopinath <gvarad2001@gmail.com>
2024-04-23 23:01:06 +0200

    add functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -1,17 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import path from 'path';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
-mongoose
-.connect(process.env.MONGO)
-.then(() => {
-    console.log("MongoDb is connected");
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("MongoDb is connected");
 });
 
 const app = express();
@@ -19,23 +17,21 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-}); 
+  console.log("Server is running on port 3000");
+});
 
+// API's
 
-
-// API's 
-
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 // Middleware
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
