ff22654b721f96e98c0eb6b0837918607384fd32
Gopinath <gvarad2001@gmail.com>
2024-05-13 13:39:01 +0100

    Completed the create a post page UI

---
+++ client/src/components/OnlyAdminPrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
+
+export default function OnlyAdminPrivateRoute() {
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser && currentUser.isAdmin ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/sign-in " />
+  ); // outlet is used to get the children Route which is present in App.jsx
+}
