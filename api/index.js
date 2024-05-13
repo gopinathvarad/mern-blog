5edf466555ae82937a12f4ae2a1bea543d954858
Gopinath <gvarad2001@gmail.com>
2024-05-13 15:10:55 +0100

    Added create a post API route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import path from "path";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
@@ -26,6 +27,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("api/post", postRoutes);
 
 // Middleware
 app.use((err, req, res, next) => {
